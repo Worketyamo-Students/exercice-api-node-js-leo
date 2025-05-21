a6e95739e09e0f5d9bfb1fce7506e5c8f218e04f
leonel691 <leonelngueaho@gmail.com>
2025-05-21 12:57:48 +0100

    exercice 4 presque finit

---
+++ exercice 4/app.js
@@ -0,0 +1,14 @@
+const express=require('express');
+const router =require('./routes/route.js')
+const bodyParser=require("body-parser")
+
+const app=express();
+const port=3004
+app.use(bodyParser.json())
+app.use(express.json())
+app.use('/', router)
+
+
+app.listen(port,()=>{
+    console.log(`le seveur tourne sur le port ${port}`)
+})
---
+++ exercice 4/controllers/controller.js
@@ -0,0 +1,90 @@
+const crypto = require("crypto");
+const fs = require("fs");
+const path = require("path");
+
+const dbPath = path.join(__dirname, "../database.json");
+
+
+function readDb() {
+    const data = fs.readFileSync(dbPath, "utf-8");
+    return JSON.parse(data);
+}
+
+function writeDb(data) { 
+    fs.writeFileSync(dbPath, JSON.stringify(data, null, 2), 'utf-8');
+}
+
+const productController = {
+    createProduct: (req, res) => {
+        const db = readDb();
+        const newProduct = {
+            id: crypto.randomBytes(3).toString('hex'),
+            name: req.body.name,
+            mark: req.body.mark
+        };
+        db.push(newProduct);
+
+        writeDb(db);
+        res.status(201).json(newProduct);
+    },
+
+    getAllProducts: (req, res) => {
+        const db = readDb();
+        res.status(200).json(db);
+    },
+
+    getProductById: (req, res) => {
+        const id = req.params.id;
+        const db = readDb();
+        const product = db.find(product => product.id === id);
+        
+        if (product) {
+            res.status(200).json(product);
+        } else {
+            res.status(404).json({ error: `l'événement avec l'ID ${id} n'existe pas` });
+        }
+    },
+
+    updateProduct: (req, res) => {
+            const id = req.params.id;
+            const newData = req.body;
+            let db = readDb();
+
+            const index = db.findIndex(product => product.id === id);
+            if (index === -1) {
+                return res.status(404).json({ message: `l'evenement dont l'id est ${id} n'esxite pas dans la bd ` });
+            }
+
+            db[index] = { ...db[index], ...newData };
+
+            if (writeDb(db)) {
+                res.json({product: db[index] });
+            } else {
+                res.status(500).json({ error: "erreur lors de la mise à jour" });
+            }
+    },
+
+    deleteProduct: (req, res) => {
+
+            const id = req.params.id;
+            let db = readDb();
+            const index = db.findIndex(product => product.id === id);
+            
+            if (index === -1) {
+                return res.status(404).json({ error: `l'événement avec l'ID ${id} n'existe pas` });
+            }
+
+            const [deletedProduct] = db.splice(index, 1);
+            
+            if (writeDb(db)) {
+                console.log("suppression de l'evenement", deletedProduct.title, "a été bien effectuée");
+                res.status(200).json({ message: "evénement supprimé avec succès" });
+            }
+    },
+    
+    getProductsWithPromos:(req,res)=>{
+        req.send('ici on verra ca apres')
+    }
+};
+
+module.exports = productController;
