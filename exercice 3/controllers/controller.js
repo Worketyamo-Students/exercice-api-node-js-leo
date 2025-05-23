89d9dfe41d7cf6699e365955e64d96b8b060420f
leonel691 <leonelngueaho@gmail.com>
2025-05-23 07:16:43 +0100

    syncronisation

--- exercice 3/controllers/controller.js
+++ exercice 3/controllers/controller.js
@@ -3,6 +3,7 @@ const fs = require("fs");
 const path = require("path");
 
 const dbPath = path.join(__dirname, "../database.json");
+const csvPath = path.join(__dirname, "../database.csv");
 
 
 function readDb() {
@@ -12,21 +13,36 @@ function readDb() {
 
 function writeDb(data) { 
     fs.writeFileSync(dbPath, JSON.stringify(data, null, 2), 'utf-8');
+    syncCSV(data)
 }
+const syncCSV = (events) => {
+  const csv = ["id,title,date"]
+    .concat(
+      events.map(
+        (event) => `${event.id},${event.title},${event.date}`
+      )
+    )
+    .join("\n");
+  fs.writeFileSync(csvPath, csv);
+};
 
 const eventController = {
     createEvent: (req, res) => {
-        const newEvent = {
-            id: db.length+1,
+        try {
+            const db = readDb();
+            const newEvent = {
+            id: crypto.randomUUID(),
             title: req.body.title,
             date: req.body.date
         };
-        const db = readDb();
         db.push(newEvent);
-        
-        if (writeDb(db)) {
-            res.status(201).json(newEvent);
-        } 
+        writeDb(db);  
+        res.status(201).json(newEvent);
+
+        } catch (error) {
+            res.status(500).json("erreur de serveur")
+            
+        }
     },
 
     getAllEvents: (req, res) => {
@@ -47,45 +63,48 @@ const eventController = {
     },
 
     updateEvent: (req, res) => {
-            const id = req.params.id;
-            const newData = req.body;
-            let db = readDb();
+            try {
+    const id = req.params.id;
+    const newData = req.body;
+    let db = readDb();
 
-            const index = db.findIndex(event => event.id === id);
-            if (index === -1) {
-                return res.status(404).json({ message: `l'evenement dont l'id est ${id} n'esxite pas dans la bd ` });
-            }
+    const index = db.findIndex(event => event.id === id);
+    if (index === -1) {
+      return res.status(404).json({ message: `L'événement avec l'ID ${id} n'existe pas.` });
+    }
 
-            db[index] = { ...db[index], ...newData };
 
-            if (writeDb(db)) {
-                res.json({event: db[index] });
-            } else {
-                res.status(500).json({ error: "erreur lors de la mise à jour" });
-            }
+    db[index] = { ...db[index], ...newData };
+    writeDb(db);
+
+    return res.status(200).json({ event: db[index] });
+    
+  } catch (error) {
+    console.error("Erreur serveur :", error);
+    return res.status(500).json({ error: "Erreur lors de la mise à jour" });
+  }
     },
 
     deleteEvent: (req, res) => {
 
-            const id = req.params.id;
-            let db = readDb();
-            const index = db.findIndex(event => event.id === id);
-            
-            if (index === -1) {
-                return res.status(404).json({ error: `l'événement avec l'ID ${id} n'existe pas` });
-            }
+        const id = req.params.id;
+        let db = readDb();
+        const index = db.findIndex(event => event.id === id);
+        
+        if (index === -1) {
+            return res.status(404).json({ error: `l'événement avec l'ID ${id} n'existe pas` });
+        }
+        const [deletedEvent] = db.splice(index, 1);
+        
+        writeDb(db);
+        console.log("suppression réussit...");
+        res.status(200).json({ message: "événement supprimé avec succès" });
 
-            const [deletedEvent] = db.splice(index, 1);
-            
-            if (writeDb(db)) {
-                console.log("suppression de l'evenement", deletedEvent.title, "a été bien effectuée");
-                res.status(200).json({ message: "evénement supprimé avec succès" });
-            }
     },
     
     compressLogs:(req,res)=>{
-        req.send('ici faudra faire de la compression')
+        res.send('ici faudra faire de la compression')
     }
 };
 
-module.exports = eventController;
+module.exports = eventController;   
